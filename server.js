8ad5bce37b5e3b4cf384e837cbb8fe3d2a3a7c5c
Krzysiu <kheciek@gmail.com>
2017-12-24 09:21:30 +0100

    new commit4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('% mongo ds143774.mlab.com:43774/testdatabase -u <chris> -p <krzysiu95>
+mongoose.connect('ds143774.mlab.com:43774/testdatabase -u <chris> -p <chris>
 ', {
     useMongoClient: true
 });
